06508f8addbc6c5ea2e34bbcaae3ea925c4cdbe9
bhanu pragnya ravikiran <ravikiran.b@northeastern.edu>
2025-10-30 02:28:48 -0400

    lkj

--- app/Labs/Lab4/ReduxExamples/CounterRedux/page.tsx
+++ app/Labs/Lab4/ReduxExamples/CounterRedux/page.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 export const dynamic = 'force-dynamic';
-export const revalidate = 0;
+
 import { useSelector, useDispatch } from "react-redux";
 import { increment, decrement } from "./counterReducer";
 export default function CounterRedux() {
