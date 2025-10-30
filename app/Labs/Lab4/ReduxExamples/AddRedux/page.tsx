bafc3a0aabb0dd581d691ba136f0a47fb9692d7a
bhanu pragnya ravikiran <ravikiran.b@northeastern.edu>
2025-10-30 02:26:34 -0400

    lkj

--- app/Labs/Lab4/ReduxExamples/AddRedux/page.tsx
+++ app/Labs/Lab4/ReduxExamples/AddRedux/page.tsx
@@ -1,7 +1,7 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 export const dynamic = 'force-dynamic';
-export const revalidate = 0;
+// export const revalidate = 0;
 
 import { useSelector, useDispatch } from "react-redux";
 import { useState } from "react";
