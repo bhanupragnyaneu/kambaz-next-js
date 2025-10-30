d362b6b76860ca04e2301b84fa83b636c0da0b15
bhanu pragnya ravikiran <ravikiran.b@northeastern.edu>
2025-10-29 23:40:20 -0400

    eslint

--- app/Labs/Lab4/ReduxExamples/todos/TodoForm.tsx
+++ app/Labs/Lab4/ReduxExamples/todos/TodoForm.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 "use client";
 import { ListGroupItem, Button, FormControl } from "react-bootstrap";
 import { useDispatch, useSelector } from "react-redux";
