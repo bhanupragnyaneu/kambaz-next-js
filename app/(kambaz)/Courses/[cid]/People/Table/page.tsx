c860ff1443cc0ed0642260260f6bc7f3ed0f1e3c
anusha chindam <anushachindam@anushas-MacBook-Air.local>
2025-12-05 01:26:35 -0500

    a6 frontend

--- app/(kambaz)/Courses/[cid]/People/Table/page.tsx
+++ app/(kambaz)/Courses/[cid]/People/Table/page.tsx
@@ -5,20 +5,34 @@ import { useSelector } from "react-redux";
 import { FaUserCircle } from "react-icons/fa";
 import * as db from "../../../../Database";
 
-export default function People() {
-  const { cid } = useParams();
-  const { enrollments } = useSelector((state: any) => state.enrollmentsReducer);
-  const { users } = db;
+import PeopleDetails from "../Details";
+import Link from "next/link";
+import { useState } from "react";
 
-  const enrolledUsers = users.filter((user: any) =>
-    enrollments.some(
-      (enrollment: any) =>
-        enrollment.course === cid && enrollment.user === user._id
-    )
-  );
+export default function People({ users = [], fetchUsers }: { users?: any[]; fetchUsers: () => void; }) {
+  const [showDetails, setShowDetails] = useState(false);
+  const [showUserId, setShowUserId] = useState<string | null>(null);
+  // const { cid } = useParams();
+  const { enrollments } = useSelector((state: any) => state.enrollmentsReducer);
+  // const { users } = db;
+
+  // const enrolledUsers = users.filter((user: any) =>
+  //   enrollments.some(
+  //     (enrollment: any) =>
+  //       enrollment.course === cid && enrollment.user === user._id
+  //   )
+  // );
 
   return (
     <div id="wd-people-table">
+           {showDetails && (
+       <PeopleDetails
+         uid={showUserId}
+         onClose={() => {
+           setShowDetails(false);
+           fetchUsers();
+         }}/>
+     )}
       <table className="table table-striped">
         <thead>
           <tr>
@@ -31,12 +45,18 @@ export default function People() {
           </tr>
         </thead>
         <tbody>
-          {enrolledUsers.map((user: any) => (
+          {users.map((user: any) => (
             <tr key={user._id}>
               <td className="wd-full-name text-nowrap">
+                <span className="text-decoration-none"
+                 onClick={() => {
+                   setShowDetails(true);
+                   setShowUserId(user._id);
+                 }} >
                 <FaUserCircle className="me-2 fs-1 text-secondary" />
                 <span className="wd-first-name">{user.firstName}</span>{" "}
                 <span className="wd-last-name">{user.lastName}</span>
+                </span>
               </td>
               <td className="wd-login-id">{user.loginId}</td>
               <td className="wd-section">{user.section}</td>
