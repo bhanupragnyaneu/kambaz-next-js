04593c056f64629a236338563a420634a572cec8
unknown <ravikiran.b@northeastern.edu>
2025-12-12 15:21:21 -0500

    frontend change for people

--- app/(kambaz)/Courses/[cid]/People/page.tsx
+++ app/(kambaz)/Courses/[cid]/People/page.tsx
@@ -1,8 +1,25 @@
 "use client"
-import { useParams, redirect } from "next/navigation";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import * as client from "../../client";
+import PeopleTable from "./Table/page";
 
 export default function People() {
-    const cid = useParams().cid;
-  return redirect(`/Courses/${cid}/People/Table`);
-  
+  const { cid } = useParams();
+  const [users, setUsers] = useState([]);
+
+  const fetchUsers = async () => {
+    try {
+      const enrolledUsers = await client.findUsersForCourse(cid as string);
+      setUsers(enrolledUsers);
+    } catch (error) {
+      console.error("Error fetching users:", error);
+    }
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, [cid]);
+
+  return <PeopleTable users={users} fetchUsers={fetchUsers} />;
 }
